d1edd51e8907de6e447b88a9a4f1d7de9c70e2a7
GlennHue <glenn.vanlooveren@student.kdg.be>
2015-05-28 15:32:28 +0200

    initial commit

--- web.js
+++ web.js
@@ -5,6 +5,7 @@ var gzippo = require('gzippo');
 var express = require('express');
 var app = express();
 
-app.use(express.logger('dev'));
 app.use(gzippo.staticGzip("" + __dirname + "/dist"));
-app.listen(process.env.PORT || 5000);
+app.listen(process.env.PORT || 5000, function() {
+  console.log('%s listening at %s', app.name, app.url);
+});
